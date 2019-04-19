8490da813c60675d9ccf44913cd12e360d2704ac
Mayura Patwardhan <mayura@email.unc.edu>
2019-04-19 10:19:52 -0400

    maya added shopify pull

--- SinglePost.js
+++ SinglePost.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react'
-import {View, Text, StyleSheet, Image, Button} from 'react-native'
+import {View, Text, StyleSheet, Image, Button, TouchableOpacity} from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
+import * as base from './environment';
+import Client from 'shopify-buy';
+import { ScrollView } from 'react-native-gesture-handler';
+
+
+const client = Client.buildClient({
+    domain: 'shopherlook.myshopify.com',
+    storefrontAccessToken: base.SHOPIFY_ACCESS_TOKEN,
+  });
 
 function TempHeader() {
     return (
@@ -8,12 +17,12 @@ function TempHeader() {
     <Text>SHOPherLOOK</Text>
     </View>);
   }
-let sampleUser = {
+const sampleUser = {
     photo: 'https://cdn1.vectorstock.com/i/1000x1000/26/45/young-executive-woman-profile-icon-vector-9692645.jpg',
     name: 'Janet Doe'
 }
 
-let sampleItem = {
+const sampleItem = {
     itemPhoto: 'https://girlgeekacademy.com/wp-content/uploads/2015/05/Sarah-Moran-Girl-Geek-Academy.jpg',
     size: 'M',
     brand: 'MPDesigns',
@@ -44,11 +53,14 @@ function NameBar({user, navigation}){
     );
 }
 
-function ImageClothes({item}){
+function ImageClothes({photo}){
     return (<Image
-    style={styles.mainImage}
-    source={{uri: item.itemPhoto}}
+      resizeMode="cover" 
+      style={styles.mainImage}
+    source={{ uri: photo }}
    />)
+
+  
 }
 
 
@@ -63,30 +75,76 @@ function Details({item}){
 }
 
 function Title({item}) {
-    return(<Text style = {styles.title}>{item.textTitle}</Text>)
+    return(<Text style = {styles.title}>{item.title}</Text>)
 }
 
 function Description({item}) {
-    return (<Text style = {styles.description}>{item.textDescription}</Text>)
+  console.log(item.description);
+    return (<Text style = {styles.description}>{item.description}</Text>)
 }
+
+
+
+
+
+
+function LookFeed(props, passed) {
+  const products = props.products;
+  const navigation = props.navigation;
+  const listProducts = products.map((product) =>
+    <Look product={product} passed = {passed} key={product.title} navigation = {navigation}></Look> 
+    )
+  
+   
+  return (
+    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+      {listProducts}
+    </ScrollView>
+  );
+}
+const Look = ({ product, navigation }, passed ) =>
+  <View>
+       <NameBar user = {sampleUser} navigation = {navigation}/>
+      <ImageClothes photo={product.images[0].src} />
+      <Title item = {product}/>
+      <Description item = {product}/>
+      <Details item = {sampleItem}/>  
+  </View>
+
+
+
+
 class SinglePost extends Component {
-    
+     constructor() {
+    super();
+
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    return client.product.fetchAll().then((res) => {
+      
+      this.setState({
+        products: res,
+        
+      });
+    }).catch(function (error) {
+      console.log('There has been a problem with your fetch operation: ' + error.message);
+      // ADD THIS THROW error
+      throw error;
+    });;
+  }
+
+
     render() {
         return(
-            <View style = {styles.container}>
-               <TempHeader/>
-               <NameBar user = {sampleUser} navigation = {this.props.navigation}/>
-              <ImageClothes item = {sampleItem}/>
-              <Title item = {sampleItem}/>
-              <Description item = {sampleItem}/>
-              <Details item = {sampleItem}/>
-              
-              
-               
-              </View>
+              <View style={styles.container}>           
+              <LookFeed products={this.state.products} passed = {this} navigation = {this.props.navigation} />
+            </View>
             
-            
-
+          
         )
     }
 }
@@ -100,8 +158,9 @@ const styles = StyleSheet.create({
       
     },
     mainImage: {
-        width: 100 + "%", 
-        height: 50 + "%"
+      resizeMode: 'cover',
+     height: 400,
+     width: 420
     },
     nameBar:{
         width: 100 + "%",
@@ -122,7 +181,7 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
     title:{
-        fontSize: 40,
+        fontSize: 30,
         marginLeft: 15,
         marginTop: 20,
         fontFamily: 'Helvetica'
@@ -140,5 +199,40 @@ const styles = StyleSheet.create({
     }
     
   });
+// const styles = StyleSheet.create({ 
+//   container: {
+//     flex: 1,
+//   },
+//   contentContainer: {
+//     paddingTop: 0,
+//   },
+//   welcomeContainer: {
+//     justifyContent: 'space-between',
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     marginTop: 50,
+//     marginBottom: 20,
+//   },
+//   influencerPhoto: {
+//     height: 50,
+//     borderRadius: 25,
+//     width: 50
+//   },
+//   lookPhoto: {
+//     resizeMode: 'cover',
+//     height: 400,
+//     width: 420
+//   },
+//   lookDescription: {
+//     flex: 1,
+//     paddingLeft: 15,
+//     paddingRight: 15,
+//     paddingBottom: 15,
+//   },
+//   lookTitle: {
+//     padding: 15,
+//     fontWeight: 'bold',
+//   },
+// });
 
 export default SinglePost
