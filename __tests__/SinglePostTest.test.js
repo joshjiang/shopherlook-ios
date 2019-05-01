07ddc953cebf5a634111973c8b37204ae2eda77d
Mayura Patwardhan <mayura@email.unc.edu>
2019-04-30 21:58:21 -0400

    added comments and tests

--- __tests__/SinglePostTest.test.js
+++ __tests__/SinglePostTest.test.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import SinglePost, {Description} from '../SinglePost.js';
+import SinglePost, {Description, Title, DetailsTitle} from '../SinglePost.js';
 import renderer from 'react-test-renderer';
 import { Text, TouchableOpacity } from 'react-native';
 
@@ -27,8 +27,8 @@ describe('App', () => {
 });
 
 describe('Description', () => {
-  it('renders the correct text', () => {
-      const description = 'Ann Taylor Petite Botanical Jacquard Button Cuff Sweater Large Petite';
+  it('renders the correct description', () => {
+      const description = 'LOFT floral dress. Color is Classic Plum. Worn a few times, just too big for me';
       const inst = renderer.create(
           <Description descriptionSplit={description} />
       );
@@ -36,5 +36,31 @@ describe('Description', () => {
       expect(
           textInst.props.children
       ).toBe(`${description}`);
+  });
+});
+
+describe('Title', () => {
+  it('renders the correct title', () => {
+      const title = 'LOFT Petite Autumn Luau Ruffle Blouson Dress Petite Medium';
+      const inst = renderer.create(
+          <Title title={title} />
+      );
+      const textInst = inst.root.findByType(Text);
+      expect(
+          textInst.props.children
+      ).toBe(`${title}`);
+  });
+});
+
+describe('DetailsHeader', () => {
+  it('renders the correct header', () => {
+      const title = 'Details';
+      const inst = renderer.create(
+          <DetailsTitle title={title} />
+      );
+      const textInst = inst.root.findByType(Text);
+      expect(
+          textInst.props.children
+      ).toBe(`${title}`);
   });
 });
